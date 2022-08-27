e7e3aa1e99f8606c220b9853f606d3387b00659f
Aynur Nasybullin <nasybullin.aynur@yandex.ru>
2022-08-28 01:01:31 +0300

    refactoring; added result message; added delegate;

--- cpp/Algorithms/Algorithms.ConsoleApp/gcd.cpp
+++ cpp/Algorithms/Algorithms.ConsoleApp/gcd.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
 #include "gcd.h"
 
-using namespace std;
-
-
 int getGCD()
 {
     int n;
     cout << "Count of numbers: ";
-
     cin >> n;
 
     int * a = new int[n];
@@ -19,20 +15,35 @@ int getGCD()
         cin >> a[i];
     }
 
+    function<int(int, int)> gcd;
+    string msg;
+
+    setGcdOptions(gcd, msg);
+
+    msg.append(to_string(a[0]));
+    for(int i = 1; i < n; i++)
+    {
+        msg.append(", ");
+        msg.append(to_string(a[i]));
+    }
+    msg.append(": ");
+
     int result = 0;
 
     for(int i = 0; i < n - 1; i++)
     {
-        result = getGCDviaDivision(a[i], a[i + 1]);
+        result = gcd(a[i], a[i + 1]);
         a[i + 1] = result;
     }
 
-    delete[] a;
+    cout << msg << result << endl;
 
-    cout << "The GCD of the entered numbers: " << result << endl;
+    delete[] a;
 
     return 0;
 }
+
+
 
 int getGCDviaDivision(int a, int b)
 {
@@ -49,4 +60,42 @@ int getGCDviaDivision(int a, int b)
     }
 
     return a + b;
+}
+
+int getGCDviaSubstraction(int a, int b)
+{
+    while(a != b)
+    {
+        if(a > b)
+        {
+            a = a - b;
+        }
+        else
+        {
+            b = b - a;
+        }
+    }
+
+    return a;
+}
+
+void setGcdOptions(function<int(int, int)> & gcd, string & msg)
+{
+    cout << "Finding the GCD via (enter of a number) ?.." << endl;
+    cout << "1) Division" << endl;
+    cout << "2) Substraction" << endl;
+
+    int option = 0;
+    cin >> option;
+
+    if(option == 0 || option == 1)
+    {
+        gcd = getGCDviaDivision;
+        msg = "The GCD (via division) of the entered numbers ";
+    }
+    else
+    {
+        gcd = getGCDviaSubstraction;
+        msg = "The GCD (via substraction) of the entered numbers ";
+    }
 }
--- cpp/Algorithms/Algorithms.ConsoleApp/gcd.h
+++ cpp/Algorithms/Algorithms.ConsoleApp/gcd.h
@@ -1,5 +1,13 @@
 #pragma once
+#include <functional>
+#include <string>
+
+using namespace std;
 
 int getGCD();
 
 int getGCDviaDivision(int a, int b);
+
+int getGCDviaSubstraction(int a, int b);
+
+void setGcdOptions(function<int(int, int)> & gcd, string & msg);

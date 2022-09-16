c0cdd1aa068de084b6ea6ff5dfde97a8aa7ccdf7
Aynur Nasybullin <nasybullin.aynur@yandex.ru>
2022-09-16 12:30:34 +0300

    refactoring iwcp ptr initialization;

--- .net/Algorithms/AlgorithmsConsoleApp/Program.cs
+++ .net/Algorithms/AlgorithmsConsoleApp/Program.cs
@@ -1,13 +1,27 @@
-﻿
+﻿using AlgorithmsConsoleApp;
 
-
-using AlgorithmsConsoleApp;
 
 Func<double, double> func;
-
 func = (x) => x * Math.Sqrt(x * x * x + 1) - x * x;
 
-(double, double) xy = OneDimensionalOptimization.UniformSearch(func, -1, 2, 0.001);
-Console.WriteLine($"Option 1: ({xy.Item1}, {xy.Item2})");
 
+int count = 1000000000;
+var startTime = System.Diagnostics.Stopwatch.StartNew();
+
+for (int i = 0; i < count; i++)
+{
+    (double, double) xy = OneDimensionalOptimization.UniformSearch(func, -1, 2, 0.001);
+    //Console.WriteLine($"iter {i}: ({xy.Item1}, {xy.Item2})");
+}
+
+startTime.Stop();
+var resultTime = startTime.Elapsed;
+
+// elapsedTime - строка, которая будет содержать значение затраченного времени
+string elapsedTime = $"{resultTime.Hours:00}:" +
+                     $"{resultTime.Minutes:00}:" +
+                     $"{resultTime.Seconds:00}." +
+                     $"{resultTime.Milliseconds:000}";
+
+Console.WriteLine($"{count} operations for {elapsedTime} time");
 
--- .net/Algorithms/AlgorithmsLibrary/OneDimensionalOptimization.cs
+++ .net/Algorithms/AlgorithmsLibrary/OneDimensionalOptimization.cs
@@ -10,20 +10,17 @@
             double currentX = left;
             double currentY = func(currentX);
 
-            //double newX = currentX + segmentLenght;
             double newY = func(currentX + segmentLenght);
 
-            int iters = 1;
-            Console.WriteLine($"{iters} (x, y): ({currentX}, {currentY})");
+            //int iters = 1;
+            //Console.WriteLine($"{iters} (x, y): ({currentX}, {currentY})");
 
             while (currentY > newY)
             {
-                iters++;
+                //iters++;
                 currentX += segmentLenght;
                 currentY = func(currentX);
                 newY = func(currentX + segmentLenght);
-                //Console.WriteLine($"(x, y): ({currentX}, {currentY})");
-                Console.WriteLine($"{iters} (x, y): ({currentX}, {currentY})");
             }
 
             return (currentX, currentY);
